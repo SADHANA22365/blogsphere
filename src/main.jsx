996f289cf94a6abbbf2dc2a0b9a5991393619966
SADHANA22365 <2416044@saec.ac.in>
2025-12-30 02:17:31 +0530

    Switch to BrowserRouter with basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
@@ -15,9 +15,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <HashRouter>
+      <BrowserRouter basename="/blogsphere">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </AuthProvider>
   </QueryClientProvider>
 );
